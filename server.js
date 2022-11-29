8b92da46c29e5c84bad435896350a58e1c0c3fbf
Satyendra Kumar <satyaku001@gmail.com>
2022-11-29 18:05:11 +0530

    uploading

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const cors = require('cors');
 //   }
 
 app.use(cors({
-  origin: "https://dropnshare.netlify.app/",
+  origin: "https://dropnshare.netlify.app/" || "https://drop-n-share.netlify.app/api/files",
   method: ["GET", "POST", "PUT"]
 }))
 app.use(express.static('public'));
